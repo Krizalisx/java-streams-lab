216a3eb0a08ac923716de8397fa8f5489ba6284d
Viktor Petrov <viktor_petrov@epam.com>
2021-10-18 11:43:43 +0300

    Lecture 5. Word statistics and pseudo-random sequences.

--- src/main/java/com/demo/InfiniteStreams.java
+++ src/main/java/com/demo/InfiniteStreams.java
@@ -7,8 +7,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import lombok.SneakyThrows;
 
 public class InfiniteStreams {
@@ -16,25 +21,44 @@ public class InfiniteStreams {
     @SneakyThrows
     public static void main(String[] args) {
         Path path = Paths.get("src/main/resources/words_alpha.txt");
-        HashSet<String> words = new HashSet<>(Files.readAllLines(path));
+        Set<String> words = new HashSet<>(Files.readAllLines(path));
+
+        Map<Integer, Long> wordsLengthByCount = words.stream().collect(Collectors.groupingBy(
+            word -> word.length(),
+            Collectors.counting()
+        ));
+        System.out.println(wordsLengthByCount);
 
         String letters = "abcdefghijklmnopqrstuvwxyz";
         char[] lettersArray = letters.toCharArray(); // [a, b, c ...]
 
-        var lettersList = letters.chars()
-            .mapToObj(c -> (char) c)
-            .map(character -> String.valueOf(character))
-            .collect(Collectors.toList());
-
-        List<String> collect = IntStream.range(0, lettersArray.length)
+        List<String> lettersList = IntStream.range(0, lettersArray.length)
             .mapToObj(i -> String.valueOf(lettersArray[i]))
             .collect(Collectors.toList());
 
         System.out.println(lettersList);
 
-        System.out.println(collect);
+        final int minLetters = 4;
+        final int maxLetters = 10;
 
-
+        Random generalRandom = new Random(43); // side effects!
+        Stream.generate(() -> generalRandom)
+            .map(random -> random.nextInt(maxLetters))
+//            .parallel()
+            .filter(i -> i >= minLetters)
+            .map(wordLength -> Stream.generate(() -> generalRandom)
+                    .map(random -> random.nextInt(lettersList.size()))
+                    .limit(wordLength)
+                    .map(i -> lettersList.get(i))
+//                .reduce("", (lhs, rhs) -> lhs + rhs)
+                    .collect(Collectors.joining())
+            )
+//            .sorted()
+            .distinct()
+            .filter(word -> words.contains(word))
+            .limit(10)
+//            .peek(System.out::println)
+            .forEachOrdered(System.out::println);
 
     }
 }
